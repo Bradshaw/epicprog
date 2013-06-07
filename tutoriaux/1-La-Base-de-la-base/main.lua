ff9e3a506580a79e1cf4ef63485da70695b0bae1
Kevin "Gaeel" Bradshaw-Rodriguez <sugardood@gmail.com>
2013-06-07 14:04:08 +0200

    Create main.lua

---
+++ tutoriaux/1-La-Base-de-la-base/main.lua
@@ -0,0 +1,19 @@
+--[[
+Code final pour le tutorial 1
+
+
+Distribué sous licence WTFPL, en gros, faites ce que vous voulez avec tout ça.
+--]]
+
+
+function love.load(arg)
+    pos_x = 250
+end
+
+function love.update(dt)
+    pos_x = pos_x + (10 * dt)
+end
+
+function love.draw()
+    love.graphics.rectangle("fill", pos_x, 200, 150, 100)
+end
